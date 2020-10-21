aa97750a2fd271430f082c37109b57fcd6e6b22e
Muhammad Arif Billah <marifbillah524@gmail.com>
2020-10-21 13:05:51 +0700

    Add files via upload

---
+++ DoWhileLoop/src/dowhileloop/DoWhileLoop.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dowhileloop;
+
+/**
+ *
+ * @author marif
+ */
+public class DoWhileLoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int angka = 1;
+        System.out.println("Angka 1 sampai 10: ");
+        do {
+        System.out.println(angka);
+        angka++;
+        }while (angka<=10);
+    }
+    
+}
---
+++ WhileLoop/src/whileloop/WhileLoop.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package whileloop;
+/**
+ *
+ * @author marif
+ */
+public class WhileLoop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int angka=1;
+        System.out.println("Angka 1 sampai 10:");
+        while(angka<=10){
+            System.out.println(angka);
+            angka++;
+        }
+    }
+    
+}
